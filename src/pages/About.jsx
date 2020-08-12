84dcdaba59135387b2b5a66cb91f116f8cc99a32
Harish Ponna <harishponna55@gmail.com>
2020-08-12 11:28:56 +0530

    done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,9 @@ import "../styles/About.css";
 function About(props) {
   return (
     <div className="About">
-      <h1>About GitFinder</h1>
+      <h1>About gity-finder</h1>
       <p>
-        GitFinder is a deceptively simple web application which searches GitHub
+        gity-finder is a deceptively simple web application which searches GitHub
         for user profiles and returns them in a responsive grid. What makes this
         app stand out from similar web developer portfolio projects is the sharp
         attention to detail and 100% from-scratch code incorporating the various
